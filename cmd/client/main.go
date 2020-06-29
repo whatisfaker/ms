50fa6edffdde398074367c71f0ec71c52db3e4df
whatisfaker <whatisfaker@gmail.com>
2020-06-29 14:31:08 +0800

    add: more comments

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,12 +8,9 @@ import (
 
 	"github.com/whatisfaker/ms/client"
 	"github.com/whatisfaker/ms/codec"
-	"github.com/whatisfaker/zaptrace/log"
-	"go.uber.org/zap"
 )
 
 func main() {
-	log := log.NewStdLogger("debug")
 	// header := []byte{'a', 'b'}
 	// data := []byte("[这里才是一个完整的数据包]")
 
@@ -22,7 +19,7 @@ func main() {
 	// sendData = append(sendData, data...)
 	fc := codec.NewLengthFieldCodec(binary.BigEndian, 1, 2, 0, 0)
 	//fc := codec.NewLineCodec()
-	for i := 1; i < 2; i++ {
+	for i := 1; i < 4; i++ {
 		go func(i int) {
 			clt := client.NewClient(
 				":3033",
@@ -36,13 +33,13 @@ func main() {
 			identity := make([]byte, 4)
 			binary.BigEndian.PutUint32(identity, uint32(i*10))
 			clt.HandleRoute(2, func(b []byte) {
-				log.Normal().Info("client recv", zap.Int("client", i), zap.Int("route", 2), zap.String("msg", string(b[3:])))
+				fmt.Println("client", i, "route", 2, string(b[3:]))
 				//fmt.Println(fmt.Sprintf("client %d route %d: %s", i, 2, string(b[3:])))
 			})
 			go func() {
 				err := clt.Start()
 				if err != nil {
-					log.Normal().Error("start error", zap.Error(err))
+					fmt.Println("start error", err)
 				}
 			}()
 			<-time.After(2 * time.Second)
@@ -50,16 +47,20 @@ func main() {
 			regist := append([]byte{1}, identity...)
 			err := clt.Send(regist)
 			if err != nil {
-				log.Normal().Error("send error 1", zap.Error(err))
+				fmt.Println("send error 1", err)
 				return
 			}
 			for {
 				<-tick.C
+				if clt.IsClosed() {
+					fmt.Println("client", i, "isClosed")
+					break
+				}
 				data := append([]byte{2}, identity...)
 				data = append(data, []byte(fmt.Sprintf("client: %d %s", i, time.Now().Format(time.RFC3339)))...)
 				err := clt.Send(data)
 				if err != nil {
-					log.Normal().Error("send error 2", zap.Error(err))
+					fmt.Println("send error 2", err)
 					break
 				}
 			}
